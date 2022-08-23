f862be2709d790e067dc60e398ba5a12836f706c
kaykaym01 <kaymoore01@gmail.com>
2022-08-23 02:16:11 -0400

    Set up initial site HTML content and react-logo image

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import reactLogo from "./react-logo.png";
 
 const rootDiv = document.createElement("div");
 rootDiv.setAttribute("id", "root");
 document.body.appendChild(rootDiv);
 
+const page = (
+    <div>
+        <img src={reactLogo} width="40px"/>
+        <h1> Fun facts about React </h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hello, React!</h1>);
+root.render(page);
