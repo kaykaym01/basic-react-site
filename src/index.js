7968c8664db413d1ed27f0359c8b1f57182a4698
kaykaym01 <kaymoore01@gmail.com>
2022-08-23 01:38:35 -0400

    Set up webpack configs for project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const body = document.body;
+const header = document.createElement("h1");
+header.textContent = "Hey, Kayla!";
+body.appendChild(header);
+
+//import React from "react"
+//ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: {
+      index: "./src/index.js",
+    },
+    devtool: "inline-source-map",
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Basic React Site",
+      }),
+    ],
+    output: {
+      filename: "index.bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+      publicPath: "",
+    },
+    devServer: {
+      static: path.resolve(__dirname, "dist"),
+      port: 8080,
+      hot: true
+    }
+}
