7f460cee09b929c9708ff4ffd8ad231dcabd4cd7
kaykaym01 <kaymoore01@gmail.com>
2022-08-23 01:53:04 -0400

    Install babel and configure babel react preset

--- webpack.config.js
+++ webpack.config.js
@@ -22,5 +22,19 @@ module.exports = {
       static: path.resolve(__dirname, "dist"),
       port: 8080,
       hot: true
-    }
+    },
+    module: {
+        rules: [
+          {
+            test: /\.?js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: ['@babel/preset-env', '@babel/preset-react']
+              }
+            }
+          },
+        ]
+      },
 }
