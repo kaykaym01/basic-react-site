fa9dd45394b1a58b7db7e71f5c1d62eeca5d7838
kaykaym01 <kaymoore01@gmail.com>
2022-08-30 21:33:17 -0400

    Separate header, footer, and main content in React components

--- src/index.js
+++ src/index.js
@@ -6,19 +6,46 @@ const rootDiv = document.createElement("div");
 rootDiv.setAttribute("id", "root");
 document.body.appendChild(rootDiv);
 
-const page = (
-    <div>
-        <img src={reactLogo} width="40px" alt="react-logo"/>
-        <h1> Fun facts about React </h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on GitHub</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
+function Header() {
+  return (
+    <header>
+      <img src={reactLogo} width="40px" alt="react-logo" />
+      <h1> Fun facts about React </h1>
+    </header>
+  );
+}
+
+function MainContent() {
+  return (
+    <main>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+       <small>© 2021 Ziroll development. All rights reserved.</small>
+    </footer>
+  );
+}
+
+function Page() {
+  return (
+    <div className="page">
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
-);
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(page);
+root.render(<Page />);
