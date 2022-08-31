aafa4233cc8b6cb2bb2fcedf2d9494b62aef0784
kaykaym01 <kaymoore01@gmail.com>
2022-08-31 10:39:34 -0400

    Add styling to main section

--- src/MainContent.js
+++ src/MainContent.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function MainContent() {
   return (
-    <main>
-      <h1> Fun facts about React </h1>
-      <ul>
+    <main className="main">
+      <h1 className="main-title"> Fun facts about React </h1>
+      <ul className="facts-list"> 
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
         <li>Has well over 100K stars on GitHub</li>
